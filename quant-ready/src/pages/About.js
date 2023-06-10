62e98278592cbb0f1e355b29f003947ee535ae84
UBC Student <armaghan18.2000@gmail.com>
2023-06-10 00:24:55 -0700

    AboutMe Page Made

--- quant-ready/src/pages/About.js
+++ quant-ready/src/pages/About.js
@@ -2,23 +2,28 @@
 import React from 'react';
 import { Box, Container, Grid, Typography, Avatar } from '@mui/material';
 import useStyles from '../styles/about.module.css';
+import armaghanImage from '../images/armaghan.jpeg'
+import stevenImage from '../images/steven.jpeg'
+import yaseerImage from '../images/yasser.jpeg'
+import angadImage from '../images/angad.jpeg'
+import annieImage from '../images/annie.jpeg'
 
 const AboutUs = () => {
   return (
     <div>
-    <Container maxWidth="lg" className={useStyles.container}>
-      {/* Add your logo image here */}
-      <img src="/path/to/your-logo.png" alt="Your Company Logo" className={useStyles.logo} />
-      <Typography variant="h2" align="center" gutterBottom>
-        About Us
-      </Typography>
-      <Grid container spacing={4} justifyContent="center">
+      <Container maxWidth="lg" className={useStyles.container}>
+        {/* Add your logo image here */}
+        <img src="/path/to/your-logo.png" alt="Your Company Logo" className={useStyles.logo} />
+        <Typography variant="h2" align="center" gutterBottom>
+          About Us
+        </Typography>
+        <Grid container spacing={4} justifyContent="center" >
         <Grid item xs={12} sm={6} md={4}>
-          <Box display="flex" flexDirection="column" alignItems="center">
+          <Box display="flex" flexDirection="column" alignItems="center" >
             {/* Add Person 1 image here */}
-            <Avatar alt="Person 1" src="/path/to/person1-image.jpg" className={useStyles.avatar} />
-            <Typography variant="h5">Person 1</Typography>
-            <Typography variant="subtitle1">Role 1</Typography>
+            <Avatar alt="Armaghan Naveed" src={armaghanImage} className={useStyles.avatar} />
+            <Typography variant="h5">Armaghan Naveed</Typography>
+            <Typography variant="subtitle1">Quant guy</Typography>
             <Typography variant="body1">
               Description about Person 1 and their role in the company.
             </Typography>
@@ -27,8 +32,8 @@ const AboutUs = () => {
         <Grid item xs={12} sm={6} md={4}>
           <Box display="flex" flexDirection="column" alignItems="center">
             {/* Add Person 2 image here */}
-            <Avatar alt="Person 2" src="/path/to/person2-image.jpg" className={useStyles.avatar} />
-            <Typography variant="h5">Person 2</Typography>
+            <Avatar alt="Steven Li" src={stevenImage} className={useStyles.avatar} />
+            <Typography variant="h5">Steven Li</Typography>
             <Typography variant="subtitle1">Role 2</Typography>
             <Typography variant="body1">
               Description about Person 2 and their role in the company.
@@ -38,8 +43,8 @@ const AboutUs = () => {
         <Grid item xs={12} sm={6} md={4}>
           <Box display="flex" flexDirection="column" alignItems="center">
             {/* Add Person 3 image here */}
-            <Avatar alt="Person 3" src="/path/to/person3-image.jpg" className={useStyles.avatar} />
-            <Typography variant="h5">Person 3</Typography>
+            <Avatar alt="Yaseer" src={yaseerImage} className={useStyles.avatar} />
+            <Typography variant="h5">Yaseer</Typography>
             <Typography variant="subtitle1">Role 3</Typography>
             <Typography variant="body1">
               Description about Person 3 and their role in the company.
@@ -50,7 +55,7 @@ const AboutUs = () => {
         <Grid item xs={12} sm={6} md={4}>
           <Box display="flex" flexDirection="column" alignItems="center">
             {/* Add Person 5 image here */}
-            <Avatar alt="Person 3" src="/path/to/person3-image.jpg" className={useStyles.avatar} />
+            <Avatar alt="Annie" src={annieImage} className={useStyles.avatar} />
             <Typography variant="h5">Person 4</Typography>
             <Typography variant="subtitle1">Role 4</Typography>
             <Typography variant="body1">
@@ -62,7 +67,7 @@ const AboutUs = () => {
         <Grid item xs={12} sm={6} md={4}>
           <Box display="flex" flexDirection="column" alignItems="center">
             {/* Add Person 5 image here */}
-            <Avatar alt="Person 3" src="/path/to/person3-image.jpg" className={useStyles.avatar} />
+            <Avatar alt="Person 3" src={angadImage} className={useStyles.avatar} />
             <Typography variant="h5">Person 5</Typography>
             <Typography variant="subtitle1">Role 5</Typography>
             <Typography variant="body1">
@@ -79,8 +84,7 @@ const AboutUs = () => {
           About Company
         </Typography>
         <Typography variant="body1">
-          Description about your company and what it offers. You can provide information about your company's history,
-          mission, values, achievements, and any other relevant details.
+        Quantready.com will be an interview preparation platform designed to assist students in preparing for quantitative finance interviews. Our platform will provide mock interviews that replicate the experience of a real-world quantitative finance interview. It’ll contain user data, question banks and external interview resources. We’ll be using interview question banks created by the UBC Trading Group & our team. Some additional functionality that we can add/remove include advanced search and filtering of questions, social sharing and collaboration and integration with Online Learning Platform.
         </Typography>
       </Box>
     </div>

8671d4a21fbb8b6cd91a01f26cd150d2f94c1c06
listeven <st3venL39@gmail.com>
2023-07-28 01:35:22 -0700

    edit proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,20 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+
 module.exports = function (app) {
+  // Proxy requests to /api and /auth/google to the backend server
   app.use(
-    ["/api", "/auth/google", "https://quantready-app.onrender.com/auth/google"],
+    ["/api", "/auth/google"],
     createProxyMiddleware({
       target: process.env.REACT_APP_API_ENDPOINT,
+      changeOrigin: true,
+    })
+  );
+
+  // Proxy requests to https://quantready-app.onrender.com/auth/google to https://quantready.onrender.com/auth/google
+  app.use(
+    "https://quantready-app.onrender.com/auth/google",
+    createProxyMiddleware({
+      target: "https://quantready.onrender.com/auth/google",
       changeOrigin: true,
     })
   );

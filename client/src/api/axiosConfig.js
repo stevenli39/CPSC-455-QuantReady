f3a2ab176cb303dce4f3fc0dead675571c33b53c
listeven <st3venL39@gmail.com>
2023-07-27 23:22:01 -0700

    change base url

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: process.env.REACT_APP_API_ENDPOINT,
 });
 
 export default axiosInstance;

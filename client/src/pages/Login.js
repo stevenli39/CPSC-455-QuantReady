e389b0ac63d6b0022aa9a6d2bc209f944088846c
Steven Li <stevenli@asana.com>
2023-07-28 18:38:21 -0700

    Changing the way we do redirects

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,11 +5,13 @@ import GoogleIcon from '@mui/icons-material/Google';
 import { Typography } from "@mui/material";
 
 function Login() {
+    const link = process.env.REACT_APP_NODE_ENV === "Development" ? "http://localhost:5000/auth/google" : "https://quantready.onrender.com/auth/google";
+
     return (
         <Container maxWidth="xl" sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh"}}>
             <h1>Sign in with Google</h1>
             <p>New users can sign up for QuantReady with Google. Existing users will be logged in.</p>
-            <a href='/auth/google'>
+            <a href={link}>
                 <Button variant="outlined" fontSize="large" startIcon={<GoogleIcon sx={{ color: "white" }} />} 
                         sx={{ backgroundColor:"#1976D2", '&:hover': {backgroundColor: "#1976D2", transform: 'scale(1.05)'} // Maintain the background color on hover 
                     }}>

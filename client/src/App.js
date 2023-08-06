93d45ad17dadd287dd6bc86284ad28ff41e19104
yassersa <yaser.snober@gmail.com>
2023-08-06 11:53:51 -0700

    added styles for nav homepage and about page and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import Router from './router.js';
 import Nav from './components/Nav.js';
-import Footer from './components/footer.js';
-
 
 function App() {
   return (
     <>
       <Nav />
       <Router />
-      <Footer />
-     
-      
     </>
   );
 }
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,153 +1,95 @@
+// AboutUs.js
 import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Fade from 'react-reveal/Fade'; // Import from react-reveal
-import steven from '../images/steven.jpeg';
-import annie from '../images/annie.jpeg';
-import yasser from '../images/yasser.jpeg';
-import angad from '../images/angad.jpeg';
-import armaghan from '../images/armaghan.jpeg';
+import { Box, Container, Grid, Typography, Avatar } from '@mui/material';
+import useStyles from '../styles/about.module.css';
+import armaghanImage from '../images/armaghan.jpeg'
+import stevenImage from '../images/steven.jpeg'
+import yaseerImage from '../images/yasser.jpeg'
+import angadImage from '../images/angad.jpeg'
+import annieImage from '../images/annie.jpeg'
 
-function AboutUsPage() {
+const AboutUs = () => {
   return (
-    <Box>
-      {/* About Us Section */}
-      <Box
-        bgcolor="rgba(0, 0, 128)" // Faded navy background
-        color="white"
-        padding="4rem"
-        textAlign="center"
-      >
-        <Typography variant="h2" sx={{ marginBottom: '2rem' }}>
+    <div>
+      <Container maxWidth="lg" className={useStyles.container}>
+        {/* Add your logo image here */}
+        <img src="/path/to/your-logo.png" alt="Your Company Logo" className={useStyles.logo} />
+        <Typography variant="h2" align="center" gutterBottom>
           About Us
         </Typography>
-        <Typography variant="body1">
-        QuantReady is more than a website; it's your dedicated partner on the path to conquering quantitative interviews. 
-        Founded with a shared passion for education and a deep understanding of the challenges these interviews pose, 
-        our team of experts is committed to equipping you with the knowledge and confidence needed to excel. 
-        Through meticulously curated content, interactive learning, and a supportive community, QuantReady is here to guide you, step by step, 
-        towards mastering the intricacies of quantitative interviews and achieving your professional aspirations.
-        </Typography>
-      </Box>
-
-      {/* Our Team Section */}
-      <Box bgcolor="white" padding="4rem" textAlign="center">
-        <Typography variant="h2" sx={{ marginBottom: '2rem' }}>
-          Our Team
-        </Typography>
-        <Grid container spacing={3}>
-          {/* Top Row */}
-          <Grid container item spacing={3} justifyContent="center">
-            {/* Card 1 */}
-            <Grid item xs={12} sm={6} md={4}>
-              <Fade bottom>
-                <Card sx={{ border: '2px solid #000080' }}>
-                  <CardMedia
-                    component="img"
-                    height={500}
-                    image={angad}
-                    alt="Team Member 1"
-                  />
-                  <CardContent>
-                    <Typography variant="h6">Angad Sudarsan</Typography>
-                    <Typography variant="body2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Fade>
-            </Grid>
-            {/* Card 2 */}
-            <Grid item xs={12} sm={6} md={4}>
-              <Fade bottom>
-                <Card sx={{ border: '2px solid #000080' }}>
-                  <CardMedia
-                    component="img"
-                    height={500}
-                    image={annie}
-                    alt="Team Member 2"
-                  />
-                  <CardContent>
-                    <Typography variant="h6">Annie Liu</Typography>
-                    <Typography variant="body2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Fade>
-            </Grid>
-          </Grid>
-
-          {/* Bottom Row */}
-          <Grid container item spacing={3} justifyContent="center" marginTop="2rem">
-            {/* Card 3 */}
-            <Grid item xs={12} sm={6} md={4}>
-              <Fade bottom>
-                <Card sx={{ border: '2px solid #000080' }}>
-                  <CardMedia
-                    component="img"
-                    height={500}
-                    image={armaghan}
-                    alt="Team Member 3"
-                  />
-                  <CardContent>
-                    <Typography variant="h6">Armaghan Naveed</Typography>
-                    <Typography variant="body2">
-                    Armaghan is a fourth-year Business and Computer Science student. He's interested in the field of Quant Trading and is amused by the growth of quantitative methods in traditional finance. Armaghan is looking forward to developing a one-stop shop for all students who are looking to get into the field of quantitative finance. In his free time, he enjoys playing soccer and watching movies.
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Fade>
-            </Grid>
-            {/* Card 4 */}
-            <Grid item xs={12} sm={6} md={4}>
-              <Fade bottom>
-                <Card sx={{ border: '2px solid #000080' }}>
-                  <CardMedia
-                    component="img"
-                    height={500}
-                    image={steven}
-                    alt="Team Member 4"
-                  />
-                  <CardContent>
-                    <Typography variant="h6">Steven Li</Typography>
-                    <Typography variant="body2">
-                    Steven is a fourth year Business and Computer science student. He's interested in Cloud infrastructure development and is  intrigued by the multitude of intriguing problems that demand creative thinking and technical expertise to overcome. Steven is excited on working on this full-stack application to help individuals better prepare for their quantitative interview. 
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Fade>
-            </Grid>
-            {/* Card 5 */}
-            <Grid item xs={12} sm={6} md={4}>
-              <Fade bottom>
-                <Card sx={{ border: '2px solid #000080' }}>
-                  <CardMedia
-                    component="img"
-                    height={500}
-                    image={yasser}
-                    alt="Team Member 5"
-                  />
-                  <CardContent>
-                    <Typography variant="h6">Yasser Sanobar</Typography>
-                    <Typography variant="body2">
-                      Yasser is a fourth year Computer Scinece student. He's interested in fullstack development and the development life cycle. He enjoys and creating new products that can help people. Yasser is excited to work on this project to help students prepare for their quantitative interviews.
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Fade>
-            </Grid>
-          </Grid>
+        <Grid container spacing={4} justifyContent="center" >
+        <Grid item xs={12} sm={6} md={4}>
+          <Box display="flex" flexDirection="column" alignItems="center" >
+            {/* Add Person 1 image here */}
+            <Avatar alt="Armaghan Naveed" src={armaghanImage} className={useStyles.avatar} />
+            <Typography variant="h5">Armaghan Naveed</Typography>
+            <Typography variant="subtitle1">Quant guy</Typography>
+            <Typography variant="body1">
+              Description about Person 1 and their role in the company.
+            </Typography>
+          </Box>
         </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            {/* Add Person 2 image here */}
+            <Avatar alt="Steven Li" src={stevenImage} className={useStyles.avatar} />
+            <Typography variant="h5">Steven Li</Typography>
+            <Typography variant="subtitle1">Role 2</Typography>
+            <Typography variant="body1">
+              Description about Person 2 and their role in the company.
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            {/* Add Person 3 image here */}
+            <Avatar alt="Yaseer" src={yaseerImage} className={useStyles.avatar} />
+            <Typography variant="h5">Yaseer</Typography>
+            <Typography variant="subtitle1">Role 3</Typography>
+            <Typography variant="body1">
+              Description about Person 3 and their role in the company.
+            </Typography>
+          </Box>
+        </Grid>
+        {/* Person 4 */}
+        <Grid item xs={12} sm={6} md={4}>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            {/* Add Person 5 image here */}
+            <Avatar alt="Annie" src={annieImage} className={useStyles.avatar} />
+            <Typography variant="h5">Person 4</Typography>
+            <Typography variant="subtitle1">Role 4</Typography>
+            <Typography variant="body1">
+              Description about Person 4 and their role in the company.
+            </Typography>
+          </Box>
+        </Grid>
+        {/* Person 5 */}
+        <Grid item xs={12} sm={6} md={4}>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            {/* Add Person 5 image here */}
+            <Avatar alt="Person 3" src={angadImage} className={useStyles.avatar} />
+            <Typography variant="h5">Person 5</Typography>
+            <Typography variant="subtitle1">Role 5</Typography>
+            <Typography variant="body1">
+              Description about Person 5 and their role in the company.
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+      </Container>
+
+
+      <Box mt={4} p={4} bgcolor="#f5f5f5">
+        <Typography variant="h4" align="center" gutterBottom>
+          About Company
+        </Typography>
+        <Typography variant="body1">
+        Quantready.com will be an interview preparation platform designed to assist students in preparing for quantitative finance interviews. Our platform will provide mock interviews that replicate the experience of a real-world quantitative finance interview. It’ll contain user data, question banks and external interview resources. We’ll be using interview question banks created by the UBC Trading Group & our team. Some additional functionality that we can add/remove include advanced search and filtering of questions, social sharing and collaboration and integration with Online Learning Platform.
+        </Typography>
       </Box>
-
-      {/* ... rest of the component ... */}
-    </Box>
+    </div>
   );
-}
+  
+};
 
-export default AboutUsPage;
+export default AboutUs;
